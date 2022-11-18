d794dd9d48b9c27e3b7b89ce37a74b7a092135b6
Julia Petrenko <juliapetrenko02.07.1999@gmai.com>
2022-11-18 08:45:16 +0200

    second

--- js/task-09.js
+++ js/task-09.js
@@ -5,13 +5,10 @@ const colorValue = document.querySelector('span.color')
 
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
 
 changeButton.addEventListener('click', e => {
   let color = getRandomHexColor();
